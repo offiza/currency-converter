dc55392644441af0cac13d003342b0242ad2577e
offiza <matveygorelik070118@gmail.com>
2022-04-13 17:45:03 +0300

    Add changeble

--- currency-converter/src/Components/CurrencyContainer/CurrencyContainer.tsx
+++ currency-converter/src/Components/CurrencyContainer/CurrencyContainer.tsx
@@ -3,26 +3,60 @@ import React, { FC, useState, useEffect } from 'react';
 import { CurrencyRow } from '../CurrencyRow/CurrencyRow';
 import axios from 'axios';
 
-const CURRENCY_URL = 'https://v6.exchangerate-api.com/v6/f0e875cd1f978365b0ce36b2/latest/UAH';
+const CURRENCY_URL = 'https://v6.exchangerate-api.com/v6/f0e875cd1f978365b0ce36b2';
 
 export const CurrencyContainer = () => {
-  const [currencies, setCurrencies] = useState<any>([]);
-  const [currencyFrom, setCurrencyFrom] = useState<any>();
-  const [currencyTo, setCurrencyTo] = useState<any>();
+  const [currencies, setCurrencies] = useState<any>([])
+  const [fromCurrency, setFromCurrency] = useState<any>()
+  const [toCurrency, setToCurrency] = useState<any>()
+  const [exchangeRate, setExchangeRate] = useState(1)
+  const [amount, setAmount] = useState(1)
+  const [amountInFromCurrency, setAmountInFromCurrency] = useState(true)
   const [error, setError] = useState('');
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount
+    toAmount = amount * exchangeRate
+  } else {
+    toAmount = amount
+    fromAmount = amount / exchangeRate
+  }
 
   useEffect(() => {
     axios
-      .get(CURRENCY_URL)
+      .get(`${CURRENCY_URL}latest/UAH`)
       .then(respone => {
+        console.log([...Object.keys(respone.data.conversion_rates)][0])
         setCurrencies([...Object.keys(respone.data.conversion_rates)]);
-        setCurrencyFrom(Object.keys(respone.data.conversion_rates[0]));
-        setCurrencyTo(Object.keys(respone.data.conversion_rates[0]));
+        setFromCurrency([...Object.keys(respone.data.conversion_rates)][0]);
+        setToCurrency([...Object.keys(respone.data.conversion_rates)][5]);
       })
       .catch((error) => {
         setError(error.message);
       })
   }, []);
+
+  useEffect(() => {
+    if (fromCurrency != null && toCurrency != null) {
+      axios(`${CURRENCY_URL}/pair/${fromCurrency}/${toCurrency}`)
+        .then(response => {
+          setExchangeRate(response.data.conversion_rate);
+        })
+    }
+  }, [fromCurrency, toCurrency])
+
+  function handleFromAmountChange(e: any) {
+    if (parseInt(e.target.value) >= 0)
+      setAmount(parseInt(e.target.value))
+    setAmountInFromCurrency(true)
+  }
+
+  function handleToAmountChange(e: any) {
+    if (parseInt(e.target.value) >= 0)
+      setAmount(parseInt(e.target.value))
+    setAmountInFromCurrency(false)
+  }
 
   return (
     <Box sx={{ width: { sm: 'min(80%, 700px)', xs: '100%' }, border: { sm: '1px solid gray', xs: 'none' }, margin: 'auto' }}>
@@ -33,8 +67,18 @@ export const CurrencyContainer = () => {
       </Box>
       {!error ?
         <Box>
-          <CurrencyRow currencies={currencies} selectedCurrency={currencyFrom} />
-          <CurrencyRow currencies={currencies} selectedCurrency={currencyTo} />
+          <CurrencyRow
+            currencies={currencies}
+            selectedCurrency={fromCurrency}
+            handleChange={(e: any) => setFromCurrency(e.target.value)}
+            amount={fromAmount}
+            handleAmount={handleFromAmountChange} />
+          <CurrencyRow
+            currencies={currencies}
+            selectedCurrency={toCurrency}
+            handleChange={(e: any) => setToCurrency(e.target.value)}
+            amount={toAmount}
+            handleAmount={handleToAmountChange} />
         </Box>
         :
         <Box sx={{ textAlign: 'center', mt: '30px' }}>
--- currency-converter/src/Components/CurrencyRow/CurrencyRow.tsx
+++ currency-converter/src/Components/CurrencyRow/CurrencyRow.tsx
@@ -4,14 +4,23 @@ import React, { FC } from 'react';
 interface CurrencyRowProps {
   currencies: any[];
   selectedCurrency: any;
+  amount: Number;
+  handleChange: (e: any) => void;
+  handleAmount: (e: any) => void
 }
 
-export const CurrencyRow: FC<CurrencyRowProps> = ({ currencies, selectedCurrency }) => {
+export const CurrencyRow: FC<CurrencyRowProps> = ({ currencies, selectedCurrency, handleChange, amount, handleAmount }) => {
 
   return (
     <Box sx={{ display: 'flex', margin: '10px' }}>
-      <TextField fullWidth variant='outlined' sx={{ marginRight: '10px' }} />
-      <select value={selectedCurrency}>
+      <TextField
+        fullWidth
+        type='number'
+        variant='outlined'
+        sx={{ marginRight: '10px' }}
+        value={amount}
+        onChange={(e) => handleAmount(e)} />
+      <select value={selectedCurrency} onChange={handleChange}>
         {currencies &&
           currencies.map(currency => {
             return currency && <option value={currency} key={currency}>{currency}</option>
